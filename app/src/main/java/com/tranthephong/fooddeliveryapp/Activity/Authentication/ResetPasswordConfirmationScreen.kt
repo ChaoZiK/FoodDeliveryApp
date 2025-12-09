4697d59573a8e3506af381e3f184e1a8c339ad38
ChaoZiK <tranthephong101@gmail.com>
2025-12-09 16:37:59 +0700

    Fixed minor warnings

--- app/src/main/java/com/tranthephong/fooddeliveryapp/Activity/Authentication/ResetPasswordConfirmationScreen.kt
+++ app/src/main/java/com/tranthephong/fooddeliveryapp/Activity/Authentication/ResetPasswordConfirmationScreen.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
@@ -93,7 +94,7 @@ fun ResetPasswordConfirmationScreen(
                 .fillMaxWidth()
                 .height(54.dp)
         ) {
-            Icon(Icons.Default.ArrowBack, contentDescription = null)
+            Icon(Icons.AutoMirrored.Filled.ArrowBack, contentDescription = null)
             Spacer(Modifier.width(8.dp))
             Text("Back to login", color = Color.Black)
         }
